8fe65e6eaa04a5504ab444b620333b15c0521828
TommyTr <TommyTran96@hotmail.com>
2024-10-27 17:29:25 -0400

    Add skeleton app.js and www to start server listening

---
+++ server/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+//import your route just like the example below
+//import  quotes from './routes/quotes.js';
+
+import path from 'path';
+const app = express();
+
+app.use(express.json());
+const __dirname = import.meta.dirname;
+app.use(express.static(path.join(__dirname, 'public')));
+
+// uses your route e.g below
+// app.use('/', quotes);
+
+///other routes
+
+app.use((req, res) => {
+  res.status(404).send('invalid path');
+});
+
+export default app;
