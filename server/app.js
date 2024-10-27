a78fbbb63c549ca5b164c1ae8a486bfdd03bdce7
TommyTr <TommyTran96@hotmail.com>
2024-10-27 18:47:42 -0400

    Remove unused codes

--- server/app.js
+++ server/app.js
@@ -2,11 +2,9 @@ import express from 'express';
 //import your route just like the example below
 //import  quotes from './routes/quotes.js';
 
-import path from 'path';
 const app = express();
 
 app.use(express.json());
-const __dirname = import.meta.dirname;
 app.use(express.static('../client/dist'));
 
 // uses your route e.g below
