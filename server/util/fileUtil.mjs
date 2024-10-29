bf1763835c0b1ce6f2fcda5aff5d911cdcb742c1
TommyTr <TommyTran96@hotmail.com>
2024-10-28 20:44:16 -0400

    Move file methods to another module

---
+++ server/util/fileUtil.mjs
@@ -0,0 +1,51 @@
+import * as fs from 'node:fs/promises';
+'use strict';
+
+/**
+ * Validates if the specified path exists.
+ *
+ * @param {string} path - The path to validate.
+ * @returns {Promise<boolean>} - A promise that resolves to true if the path exists,
+ * false otherwise.
+ */
+
+async function validatePath(path) {
+  try {
+    await fs.access(path);
+    return true;
+  } catch (e) {
+    console.error(e.message);
+    return false;
+  }
+}
+
+/**
+ * Checks if the specified path is a file.
+ *
+ * @param {string} path - The path to check.
+ * @returns {Promise<boolean>} - A promise that resolves to true if the path is a file, 
+ * false otherwise.
+ */
+async function isFile(path) {
+  try {
+    const stats = await fs.stat(path);
+    return stats.isFile();
+  } catch (e) {
+    console.error(e.message);
+    return false;
+  }
+}
+
+async function readData(path) {
+  try {
+    if (await validatePath(path) && await isFile(path)) {
+      const data = await fs.readFile(path, 'utf-8');
+      const dataJson = JSON.parse(data);
+      return dataJson;
+    }
+  } catch (error) {
+    throw Error('error reading file : ' + error);
+  }
+};
+
+export { validatePath, isFile, readData };
