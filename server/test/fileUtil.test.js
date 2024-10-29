8386b8b8beddf92ebc6678f892cf2ab746e5a644
TommyTr <TommyTran96@hotmail.com>
2024-10-28 21:27:52 -0400

    Change test description

--- server/test/fileUtil.test.js
+++ server/test/fileUtil.test.js
@@ -6,12 +6,13 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 describe('validatePath', () => {
-  it('should return true when file exists', async () => {
+  it('should return true when path exists', async () => {
     const result = await fileUtil.validatePath('./test/fakeFile.txt');
     expect(result).to.be.true;
   });
+
   it('should return false when file does not exist', async () => {
-    const result = await fileUtil.validatePath('./test/nofiledandwajdwadaw.txt');
+    const result = await fileUtil.validatePath('./void/nofiledandwajdwadaw.txt');
     expect(result).to.be.false;
   });
 });
