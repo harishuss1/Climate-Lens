5cbed71c6c496ff1a341d8ab7c4e02b1b1963d8c
TommyTr <TommyTran96@hotmail.com>
2024-10-29 19:50:32 -0400

    Fix connection error from gitlabci

--- server/db/db.js
+++ server/db/db.js
@@ -9,23 +9,24 @@ class DB {
     //instance is the singleton, defined in outer scope
     if (!instance) {
       instance = this;
-      this.mongoClient = new MongoClient(dbUrl, {
-        serverApi: {
-          version: ServerApiVersion.v1,
-          strict: true,
-          deprecationErrors: true,
-        }
-      });
+      this.mongoClient = null;
       this.db = null;
       this.collection = null;
     }
     return instance;
   }
-
   async connect(dbname, collName) {
-    if (instance.db){
+    if (instance.db) {
       return;
     }
+    this.mongoClient = new MongoClient(dbUrl, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    });
+    
     await instance.mongoClient.connect();
     instance.db = await instance.mongoClient.db(dbname);
     // Send a ping to confirm a successful connection
@@ -47,11 +48,15 @@ class DB {
     instance = null;
   }
 
+  isInstanceNull() {
+    return instance === null;
+  }
+
   async readAll() {
     return await instance.collection.find().toArray();
   }
 
-  async createMany(quotes){
+  async createMany(quotes) {
     return await instance.collection.insertMany(quotes);
   }
 
