8605ad0a6f3b42352ba66af0311fac07e26d787b
aholmesohone <aholmesohone@gmail.com>
2024-10-28 22:37:39 -0400

    add emissions router

---
+++ server/routers/emissions.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { getEmissionData } from '../controllers/emissionsController.mjs';
+
+const router = express.Router();
+
+// GET /api/temp/:country/:year?
+router.get('/:country/:year?', (req, res, next) => {
+  if(!isNaN(req.params.country) && req.params.country.length === 4){
+    next();
+  }
+  getEmissionData(req, res);
+});
+
+router.get('/:year/:country?', getEmissionData);
+
+export default router;
