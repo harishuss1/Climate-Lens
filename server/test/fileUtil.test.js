b807e2c7ebbb25f20b68711e9f63d86fe9b7b504
TommyTr <TommyTran96@hotmail.com>
2024-10-28 21:31:09 -0400

    Add unit test for isFile method

--- server/test/fileUtil.test.js
+++ server/test/fileUtil.test.js
@@ -5,7 +5,7 @@ import chaiAsPromised from 'chai-as-promised';
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
-describe('validatePath', () => {
+describe('validatePath method', () => {
   it('should return true when path exists', async () => {
     const result = await fileUtil.validatePath('./test/fakeFile.txt');
     expect(result).to.be.true;
@@ -13,6 +13,18 @@ describe('validatePath', () => {
 
   it('should return false when file does not exist', async () => {
     const result = await fileUtil.validatePath('./void/nofiledandwajdwadaw.txt');
+    expect(result).to.be.false;
+  });
+});
+
+describe('isFile method', () => {
+  it('should return true if it is a file', async () => {
+    const result = await fileUtil.isFile('./test/fakeFile.txt');
+    expect(result).to.be.true;
+  });
+
+  it('should return false if it is not a file', async () => {
+    const result = await fileUtil.isFile('./test/directory');
     expect(result).to.be.false;
   });
 });
