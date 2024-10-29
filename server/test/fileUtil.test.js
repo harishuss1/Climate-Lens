5a99462e624e9ecf4409480d5df8f7667aa57cdf
TommyTr <TommyTran96@hotmail.com>
2024-10-28 21:24:05 -0400

    Add test for validatePath method

---
+++ server/test/fileUtil.test.js
@@ -0,0 +1,17 @@
+import * as fileUtil from '../util/fileUtil.mjs';
+import * as chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe('validatePath', () => {
+  it('should return true when file exists', async () => {
+    const result = await fileUtil.validatePath('./test/fakeFile.txt');
+    expect(result).to.be.true;
+  });
+  it('should return false when file does not exist', async () => {
+    const result = await fileUtil.validatePath('./test/nofiledandwajdwadaw.txt');
+    expect(result).to.be.false;
+  });
+});
