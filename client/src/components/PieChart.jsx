0d62e7f0318b191e827c48de269337cc5c37d4b4
TommyTr <TommyTran96@hotmail.com>
2024-11-08 22:47:45 -0500

    Add percentage in legend table for each legend item

--- client/src/components/PieChart.jsx
+++ client/src/components/PieChart.jsx
@@ -3,8 +3,8 @@
 import { Chart as ChartJS } from 'chart.js/auto';
 import { Pie } from 'react-chartjs-2';
 
-//name to be changed?
-export default function PieChart1({ data }) {
+
+export default function PieChart({ data }) {
   if (!data || data.length === 0) {
     return <h2>no data</h2>;
   }
@@ -14,22 +14,50 @@ export default function PieChart1({ data }) {
     key !== 'Total' && key !== 'Country' && key !== 'Year' && key !== '_id'
   );
 
+  const totalEmissions = data[0]['Total'];
+
+  const percentageLabels = labels.map((label, index) => {
+    const percentage = calculatePercentage(filteredData[index], totalEmissions);
+    return `${label} (${percentage}%)`;
+  });
+
+  function calculatePercentage(value, total) {
+    return ((value / total) * 100).toFixed(1);
+  }
+
+
+  const filteredData = labels.map(label => data[0][label]);
+
+
   return (
     <Pie
       data={{
-        labels: labels,
+        labels: percentageLabels,
         datasets: [
           {
-            //for each key, get its value 
-            data: labels.map(label => data[0][label]),
-            backgroundColor: labels.map((label, index) =>
-              `rgba(${Math.floor(Math.random() * 256)}, 
-            ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
-            ),
+            data: filteredData,
+            backgroundColor: ['red', 'blue', 'green', 'yellow', 'orange', 'violet', 'pink'],
             borderWidth: 1,
           }
         ]
       }}
+      options={{
+        plugins: {
+          //Configuration for the legend table
+          legend: {
+            position: 'right',
+            title: {
+              display: true,
+              text: 'Type of emissions',
+              font: {
+                size: 15,
+                weight: 'bold',
+              }
+            },
+          }
+        }
+      }
+      }
     />
   );
 }
