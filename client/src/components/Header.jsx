294255d3e53a3c2f9983aeb100f470b9fcd62932
Haris Hussain <hussainharis170@gmail.com>
2024-11-09 20:00:28 -0500

    add simple header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div>
+      <header className="header">
+        <h1 className="title">Climate Data Visualization Project</h1>
+      </header>
+    </div>
+  );
+}
