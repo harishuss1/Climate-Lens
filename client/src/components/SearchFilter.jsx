2db1e53b1b7e61428266cc9e297fd3bb43f1ec48
Haris Hussain <hussainharis170@gmail.com>
2024-11-10 11:31:25 -0500

    Fix exclude country to work with View1

--- client/src/components/SearchFilter.jsx
+++ client/src/components/SearchFilter.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import countries from '../../data/countries.json';
 
-export default function SearchFilter({ setCountry, setIsValid, excludedCountries }) {
+export default function SearchFilter({ setCountry, setIsValid, excludedCountries = [] }) {
   const [searchCountry, setSearchCountry] = useState('');
   const [filteredCountries, setFilteredCountries] = useState([]);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
